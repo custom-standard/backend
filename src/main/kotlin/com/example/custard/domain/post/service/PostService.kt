40e5fe5aabaf1c48ca202dc939364c88e0343d69
rnignon <rnignon@naver.com>
2024-04-29 01:57:29 +0900

    feat: Post Controller 구현 및 Service 로직 수정

--- src/main/kotlin/com/example/custard/domain/post/service/PostService.kt
+++ src/main/kotlin/com/example/custard/domain/post/service/PostService.kt
@@ -1,10 +1,13 @@
 package com.example.custard.domain.post.service
 
 import com.example.custard.domain.post.dto.info.*
+import com.example.custard.domain.post.dto.response.PostResponse
 import com.example.custard.domain.post.model.Category
 import com.example.custard.domain.post.model.Post
 import com.example.custard.domain.post.model.PostType
 import com.example.custard.domain.post.model.date.PostDate
+import com.example.custard.domain.post.service.category.CategoryStore
+import com.example.custard.domain.post.service.date.PostDateStore
 import com.example.custard.domain.user.model.User
 import com.example.custard.domain.user.service.UserStore
 import jakarta.transaction.Transactional
@@ -14,13 +17,17 @@ import java.time.LocalDate
 @Service
 class PostService(
     private val postStore: PostStore,
-    private val userStore: UserStore
+    private val postDateStore: PostDateStore,
+    private val userStore: UserStore,
+    private val categoryStore: CategoryStore
 ) {
     /* 게시글 전체 조회 */
-    fun getPosts(info: PostReadInfo): List<Post> {
+    fun getPosts(userUUID: String?, info: PostReadInfo): List<PostResponse> {
+        val user: User? = userUUID?.let { userStore.getByUUID(it) }
         val type: PostType = info.type
 
-        val category: Category? = info.category
+        val category: Category? = info.categoryId?.let { categoryStore.getCategory(it) }
+
         val date: LocalDate? = info.date
         val minPrice: Int? = info.minPrice
         val maxPrice: Int? = info.maxPrice
@@ -28,28 +35,34 @@ class PostService(
         return when (type) {
             PostType.PURCHASE -> postStore.findAllPurchasePost(category, date, minPrice, maxPrice)
             PostType.SALE -> postStore.findAllSalePost(category, date, minPrice, maxPrice)
-        }
+        }.map { post -> PostResponse.of(post) }
     }
 
     /* 게시글 생성 */
     @Transactional
-    fun createPost(email: String, info: PostCreateInfo): Post {
-        val writer: User = userStore.getByEmail(email)
-        // TODO: PostDate 처리
+    fun createPost(userUUID: String, info: PostCreateInfo): Post {
+        val writer: User = userStore.getByUUID(userUUID)
+        val category: Category = categoryStore.getCategory(info.categoryId)
 
-        val post: Post = PostCreateInfo.toEntity(info, writer)
+        val post: Post = PostCreateInfo.toEntity(info, category, writer)
+
+        val dates = info.dates
+        val postDates = dates.map { date -> PostDateInfo.toEntity(date, post) }
+
+        postDateStore.savePostDate(post, postDates)
+
         return postStore.savePost(post)
     }
 
-    fun updateDates(post: Post, startDate: LocalDate, endDate: LocalDate): MutableList<PostDate> {
-        // TODO: PostDate 처리
-        return mutableListOf()
+    fun updateDates(post: Post, dates: List<PostDateInfo>): List<PostDate> {
+        val postDates = dates.map { date -> PostDateInfo.toEntity(date, post) }
+        return postDateStore.updatePostDate(post, postDates)
     }
 
     /* 게시글 수정 */
     @Transactional
-    fun updatePost(email: String, info: PostUpdateInfo): Post {
-        val writer: User = userStore.getByEmail(email)
+    fun updatePost(userUUID: String, info: PostUpdateInfo): Post {
+        val writer: User = userStore.getByUUID(userUUID)
 
         val id: Long = info.id
         val post: Post = postStore.getById(id)
@@ -59,12 +72,10 @@ class PostService(
             throw RuntimeException("해당 게시글의 수정 권한이 없습니다.")
         }
 
-        val type: PostType = info.type
-        val category: Category = info.category
+        val category: Category = categoryStore.getCategory(info.categoryId)
         val title: String = info.title
         val description: String = info.description
-        val startDate: LocalDate = info.startDate
-        val endDate: LocalDate = info.endDate
+        val dates: List<PostDateInfo> = info.dates
         val delivery: Boolean = info.delivery
         val place: String? = info.place
         val minPrice: Int = info.minPrice
@@ -73,15 +84,15 @@ class PostService(
         val product: String? = info.product
 
         post.updatePost(category, title, description, delivery, place, minPrice, maxPrice, product)
-        updateDates(post, startDate, endDate)
+        updateDates(post, dates)
 
         return post
     }
 
     /* 게시글 삭제 */
     @Transactional
-    fun deletePost(email: String, id: Long) {
-        val writer: User = userStore.getByEmail(email)
+    fun deletePost(userUUID: String, id: Long) {
+        val writer: User = userStore.getByUUID(userUUID)
         val post: Post = postStore.getById(id)
 
         if (post.writer.id != writer.id) {
@@ -89,6 +100,7 @@ class PostService(
             throw RuntimeException("해당 게시글의 삭제 권한이 없습니다.")
         }
 
+        postDateStore.deleteAllByPost(post)
         postStore.deletePost(post)
     }
 }
