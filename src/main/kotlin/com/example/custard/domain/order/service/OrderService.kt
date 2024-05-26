a4ba8c8800a26e6fa58d9af30f2d48e71e309c74
rnignon <rnignon@naver.com>
2024-05-27 02:10:40 +0900

    feat: Order Service 메서드 수정 및 관련 클래스 반영

--- src/main/kotlin/com/example/custard/domain/order/service/OrderService.kt
+++ src/main/kotlin/com/example/custard/domain/order/service/OrderService.kt
@@ -1,13 +1,21 @@
 package com.example.custard.domain.order.service
 
+import com.example.custard.domain.order.dto.info.*
+import com.example.custard.domain.order.dto.response.*
+import com.example.custard.domain.order.enums.OrderPosition
+import com.example.custard.domain.order.enums.OrderStatus
 import com.example.custard.domain.order.model.Order
+import com.example.custard.domain.post.model.Post
+import com.example.custard.domain.post.service.PostStore
 import com.example.custard.domain.user.model.User
 import com.example.custard.domain.user.service.UserStore
+import org.springframework.data.domain.Page
 import org.springframework.stereotype.Service
 
 @Service
 class OrderService (
     private val orderStore: OrderStore,
+    private val postStore: PostStore,
     private val userStore: UserStore,
 ) {
     private fun validateUser(user: User, order: Order) {
@@ -17,21 +25,24 @@ class OrderService (
         }
     }
 
-    fun getOrder(userUUID: String, orderId: Long): OrderDetailResponse {
+    fun getOrder(userUUID: String, orderId: Long): OrderResponse {
         val user: User = userStore.getByUUID(userUUID)
         val order: Order = orderStore.getById(orderId)
 
         validateUser(user, order)
 
-        return OrderDetailResponse.of(order)
+        return OrderResponse.of(order, order.isRequester(user))
     }
 
     // 내 거만 (필터: status, 카테고리, 판매자/구매자) 아니면 게시글에 따라서 필터
-    fun getOrders(userUUID: String, info: OrderReadInfo): List<OrderResponse> {
+    fun getOrders(userUUID: String, info: OrderReadInfo): Page<OrderResponse> {
         val user: User = userStore.getByUUID(userUUID)
-        val order: Order? = orderStore.findById(info.orderId)
+        val post: Post? = info.postId?.let { postStore.getById(it) }
 
-        if (order != null) { validateUser(user, order) }
+        if (post != null && !post.isWriter(user)) {
+            // TODO: Exception 처리
+            throw RuntimeException("해당 게시물에 대한 작성자가 아닙니다.")
+        }
 
         val page: Int = info.page
         val size: Int = info.size
@@ -40,30 +51,51 @@ class OrderService (
         // 필터 조건
         val status: OrderStatus? = info.status
 
-        val orders: List<Order> = orderStore.getOrders(page, size, position, user, order, status)
-        return orders.map { OrderResponse.of(it) }
+        val orders: Page<Order> = orderStore.getOrders(page, size, position, user, post, status)
+        return orders.map { OrderResponse.of(it, it.isRequester(user)) }
     }
 
-    fun createOrder(userUUID: String, info: OrderCreateInfo): OrderDetailResponse {
-        val requester: User = userStore.getByUUID(userUUID)
-        val responder: User = userStore.getByUUID(info.otherUUID)
+    fun createOrder(userUUID: String, info: OrderCreateInfo): OrderResponse {
+        val post: Post = postStore.getById(info.postId)
 
-        val order: Order = OrderCreateInfo.toEntity(info, requester, responder)
+        val requester: User = userStore.getByUUID(userUUID)
+        val responder: User = post.writer
+
+        val order: Order = OrderCreateInfo.toEntity(info, post, requester, responder)
 
         orderStore.saveOrder(order)
 
-        return OrderDetailResponse.of(order)
+        return OrderResponse.of(order, order.isRequester(requester))
     }
 
-    fun updateOrderStatus(userUUID: String, info: OrderUpdateStatusInfo): OrderDetailResponse {
+    fun respondOrder(userUUID: String, orderId: Long, accept: Boolean): OrderResponse {
+        val user: User = userStore.getByUUID(userUUID)
+        val order: Order = orderStore.getById(orderId)
+
+        order.respondOrder(user, accept)
+
+        return OrderResponse.of(order, order.isRequester(user))
+    }
+
+    fun updateOrderStatus(userUUID: String, info: OrderUpdateStatusInfo): OrderResponse {
         val user: User = userStore.getByUUID(userUUID)
         val order: Order = orderStore.getById(info.orderId)
 
         validateUser(user, order)
 
-        order.updateStatus(info.status)
+        if (info.forward) { order.forwardStatus() } else { order.backwardStatus() }
 
-        return OrderDetailResponse.of(order)
+        return OrderResponse.of(order, order.isRequester(user))
+    }
+
+    fun updateOrderData(userUUID: String, info: OrderUpdateDataInfo): OrderResponse {
+        val user: User = userStore.getByUUID(userUUID)
+        val order: Order = orderStore.getById(info.orderId)
+
+        validateUser(user, order)
+        // TODO: Order에 대해 승낙된 Propose 검증 후 Order 수정
+
+        return OrderResponse.of(order, order.isRequester(user))
     }
 
     fun deleteOrder(userUUID: String, orderId: Long) {
