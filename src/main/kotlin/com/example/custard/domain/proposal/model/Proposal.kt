f29a7d0889238360ab1ed92a5e827c2f11daeb49
rnignon <rnignon@naver.com>
2024-07-22 22:36:56 +0900

    feat: Proposal Status 추가

--- src/main/kotlin/com/example/custard/domain/proposal/model/Proposal.kt
+++ src/main/kotlin/com/example/custard/domain/proposal/model/Proposal.kt
@@ -2,6 +2,8 @@ package com.example.custard.domain.proposal.model
 
 import com.example.custard.domain.order.model.Order
 import com.example.custard.domain.common.date.Date
+import com.example.custard.domain.proposal.enums.ProposalStatus
+import com.example.custard.domain.proposal.exception.InvalidProposalStatusException
 import com.example.custard.domain.user.model.User
 import jakarta.persistence.*
 
@@ -30,10 +32,17 @@ class Proposal(
     @ManyToOne(fetch = FetchType.LAZY)
     val date: Date = date
 
-    var accept: Boolean = false
-        protected set
+    var status: ProposalStatus = ProposalStatus.PENDING
 
-    fun acceptProposal() {
-        accept = true
+    fun updateProposalStatus(accept: Boolean) {
+        validateIsPending()
+
+        status = if (accept) ProposalStatus.ACCEPTED else ProposalStatus.REJECTED
+    }
+
+    fun validateIsPending() {
+        if (status != ProposalStatus.PENDING) {
+            throw InvalidProposalStatusException("이미 처리된 제안입니다.")
+        }
     }
 }
