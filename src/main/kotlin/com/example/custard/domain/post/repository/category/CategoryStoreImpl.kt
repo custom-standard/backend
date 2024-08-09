3a9f9dbb766ec78d180887c9265b54c3e57844a2
rnignon <rnignon@naver.com>
2024-08-09 15:29:16 +0900

    feat: Category Exception 구현

--- src/main/kotlin/com/example/custard/domain/post/repository/category/CategoryStoreImpl.kt
+++ src/main/kotlin/com/example/custard/domain/post/repository/category/CategoryStoreImpl.kt
@@ -1,5 +1,6 @@
 package com.example.custard.domain.post.repository.category
 
+import com.example.custard.domain.post.exception.NoSuchCategoryException
 import com.example.custard.domain.post.model.Category
 import com.example.custard.domain.post.service.category.CategoryStore
 import org.springframework.stereotype.Component
@@ -14,8 +15,7 @@ class CategoryStoreImpl(
     }
 
     override fun getCategory(categoryId: Long): Category {
-        // TODO: Exception handling
         return categoryRepository.findById(categoryId)
-            .orElseThrow { RuntimeException("존재하지 않는 카테고리입니다.") }
+            .orElseThrow { NoSuchCategoryException("존재하지 않는 카테고리입니다.") }
     }
 }
