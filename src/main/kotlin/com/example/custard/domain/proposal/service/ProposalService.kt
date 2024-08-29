f03886e69bc5f69e487727e4a3fb4e1aea35b1ca
rnignon <rnignon@naver.com>
2024-08-29 22:03:59 +0900

    feat: Proposal 내 ProposalImage 파일 서비스 연동 후 파일 저장 구현

--- src/main/kotlin/com/example/custard/domain/proposal/service/ProposalService.kt
+++ src/main/kotlin/com/example/custard/domain/proposal/service/ProposalService.kt
@@ -1,20 +1,25 @@
 package com.example.custard.domain.proposal.service
 
+import com.example.custard.domain.common.file.File
+import com.example.custard.domain.common.file.FileStore
 import com.example.custard.domain.order.model.Order
 import com.example.custard.domain.order.service.OrderStore
 import com.example.custard.domain.proposal.dto.info.ProposalCreateInfo
 import com.example.custard.domain.proposal.dto.response.ProposalResponse
 import com.example.custard.domain.proposal.model.Proposal
+import com.example.custard.domain.proposal.model.ProposalImage
 import com.example.custard.domain.user.model.User
 import com.example.custard.domain.user.service.UserStore
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.multipart.MultipartFile
 
 @Service
 class ProposalService (
     private val proposalStore: ProposalStore,
     private val orderStore: OrderStore,
     private val userStore: UserStore,
+    private val fileStore: FileStore,
 ) {
     fun getProposals(userUUID: String, orderId: Long): List<ProposalResponse> {
         val order: Order = orderStore.getById(orderId)
@@ -35,14 +40,24 @@ class ProposalService (
 
 
     @Transactional
-    fun createProposal(userUUID: String, info: ProposalCreateInfo): ProposalResponse {
+    fun createProposal(userUUID: String, info: ProposalCreateInfo, files: List<MultipartFile>): ProposalResponse {
         val order: Order = orderStore.getById(info.orderId)
         val sender: User = userStore.getByUUID(userUUID)
         val receiver: User = order.getOtherUser(sender)
 
-        val proposal: Proposal = ProposalCreateInfo.toEntity(info, order, sender, receiver)
+        val proposal: Proposal = proposalStore.saveProposal(ProposalCreateInfo.toEntity(info, order, sender, receiver))
+        storeImages(proposal, files)
 
         return ProposalResponse.of(proposalStore.saveProposal(proposal))
+    }
+
+    private fun storeImages(proposal: Proposal, files: List<MultipartFile>) {
+        val imagePath: String = "proposal/${proposal.id}"
+        val images: List<File> = fileStore.uploadFiles(imagePath, files)
+
+        val proposalImages: List<ProposalImage> = images.map { ProposalImage(proposal, it) }
+
+        proposal.updateImages(proposalImages.toMutableList())
     }
 
     @Transactional
