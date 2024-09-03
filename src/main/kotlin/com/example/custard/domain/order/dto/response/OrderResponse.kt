adb5b89c03ffd8423a5404a64a93d7c648eab6a0
rnignon <rnignon@naver.com>
2024-09-03 21:36:36 +0900

    feat: Order 간소화 응답 추가 및 Order 응답 수정

--- src/main/kotlin/com/example/custard/domain/order/dto/response/OrderResponse.kt
+++ src/main/kotlin/com/example/custard/domain/order/dto/response/OrderResponse.kt
@@ -1,5 +1,6 @@
 package com.example.custard.domain.order.dto.response
 
+import com.example.custard.domain.common.file.FileResponse
 import com.example.custard.domain.order.enums.OrderStatus
 import com.example.custard.domain.order.model.Order
 import com.example.custard.domain.post.dto.response.PostSimpleResponse
@@ -10,6 +11,8 @@ class OrderResponse (
     val post: PostSimpleResponse,
     val requester: UserResponse,
     val responder: UserResponse,
+    val requestMessage: String,
+    val images: List<FileResponse>,
     val price: Int,
     val dates: List<OrderDateResponse>,
     val status: OrderStatus,
@@ -18,11 +21,14 @@ class OrderResponse (
     companion object {
         fun of(order: Order, isRequest: Boolean): OrderResponse {
             val dates: List<OrderDateResponse> = order.dates.map { OrderDateResponse.of(it) }
+            val images: List<FileResponse> = order.images.map { FileResponse.of(it.file) }
             return OrderResponse(
                 orderId = order.id,
                 post = PostSimpleResponse.of(order.post),
                 requester = UserResponse.of(order.requester),
                 responder = UserResponse.of(order.responder),
+                requestMessage = order.requestMessage,
+                images = images,
                 price = order.price,
                 dates = dates,
                 status = order.status,
