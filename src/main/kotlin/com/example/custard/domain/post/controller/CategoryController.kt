b3ff2617bee9e7136974763918666895f30c6b3b
rnignon <rnignon@naver.com>
2024-09-22 13:54:34 +0900

    fix: Controller 누락사항 추가

--- src/main/kotlin/com/example/custard/domain/post/controller/CategoryController.kt
+++ src/main/kotlin/com/example/custard/domain/post/controller/CategoryController.kt
@@ -4,7 +4,9 @@ import com.example.custard.domain.post.dto.request.category.CategoryCreateReques
 import com.example.custard.domain.post.dto.request.category.CategoryUpdateRequest
 import com.example.custard.domain.post.dto.response.CategoryResponse
 import com.example.custard.domain.post.service.category.CategoryService
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -28,12 +30,14 @@ class CategoryController (
         return categoryService.createCategory(request.createInfo())
     }
 
+    @PatchMapping
     fun updateCategory(
         @RequestBody request: CategoryUpdateRequest
     ): CategoryResponse {
         return categoryService.updateCategory(request.categoryId, request.createInfo())
     }
 
+    @DeleteMapping
     fun deleteCategory(categoryId: Long) {
         categoryService.deleteCategory(categoryId)
     }
